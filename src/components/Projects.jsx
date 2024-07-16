4442a2e9dd95c1257a8477040b4b3e39067d56c3
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-07-16 19:25:14 -0400

    Add Stock Market Portfolio project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,15 +4,24 @@ import Project from './Project';
 const projects = [
     {
         id: 1,
-        name: 'E-Commerce-Platform',
+        name: 'E-Commerce Platform',
         tools: ['NodeJS', 'Express', 'MongoDB', 'React', 'CSS'],
         img: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1719608700/Portfolio%20Projects/e-commerce-demo_e18wss.gif`,
-        demo: 'https://e-commerce-mern-fju7.onrender.com',
-        github: 'https://github.com/cnourrcier',
+        demo: 'https://cardinalfinds.com',
+        github: 'https://github.com/cnourrcier/e-commerce-mern',
         description: 'A complete, robust, and user-friendly e-commerce application that includes authentication and authorization, data validations, product search, product browsing, product management, shopping cart functionality, checkout and order processing, and mock payment simulation.'
     },
     {
         id: 2,
+        name: 'Stock Market Portfolio',
+        tools: ['NodeJS', 'Express', 'MongoDB', 'React', 'MaterialUI'],
+        img: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1721171379/Portfolio%20Projects/stock-market-portfolio_dos4by.gif`,
+        demo: 'https://stock-market-portfolio-mern.onrender.com',
+        github: 'https://github.com/cnourrcier/stock-market-portfolio-mern',
+        description: 'A Full Stack MERN web application that displays stock prices, allows the user to filter/sort, and add/remove stocks from a watchlist'
+    },
+    {
+        id: 3,
         name: 'Image Search Gallery',
         tools: ['React', 'Tailwind CSS', 'Pixabay API'],
         img: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1719695190/Portfolio%20Projects/imagesearchgallery_ibw6wi.gif`,
@@ -21,7 +30,7 @@ const projects = [
         description: 'A React-based project for searching and displaying images, including a user-friendly search interface and detailed image cards with metadata and tag-based search functionality.'
     },
     {
-        id: 3,
+        id: 4,
         name: 'Expense Tracker',
         tools: ['NodeJS', 'Express', 'MongoDB', 'React'],
         img: `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_PRODUCT_ENV}/image/upload/v1719692435/Portfolio%20Projects/expensetracker_sko0s5.gif`,
